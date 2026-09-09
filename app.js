ecf223f0110fb9bc37702209703a519d47104fc5
Md Anees Khan <khanmdanis145@gmail.com>
2026-09-09 07:33:09 +0000

    A2: Stage 3: update and delete with SQL Done

--- app.js
+++ app.js
@@ -13,25 +13,6 @@ const Port = process.env.PORT;
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Tasks array to store the tasks
-let tasks = [
-  {
-    id: 1,
-    title: "Learn HTTP",
-    done: false,
-  },
-  {
-    id: 2,
-    title: "Build API",
-    done: false,
-  },
-  {
-    id: 3,
-    title: "Push to GitHub",
-    done: false,
-  },
-];
-
 // Get routes
 app.get("/", (req, res) => {
   res.json({
@@ -75,26 +56,27 @@ app.post("/tasks", (req, res) => {
       error: "Title is required",
     });
   }
+  const cleanTitle = title.trim();
   const stmt = db.prepare("INSERT INTO tasks (title, done) VALUES(?,?)");
-  const result = stmt.run(title, 0);
+  const result = stmt.run(cleanTitle, 0);
 
   const newTask = {
     id: result.lastInsertRowid,
-    title: title.trim(),
+    title: cleanTitle,
     done: false,
   };
 
   res.status(201).json(newTask);
-  });
+});
 
 // Put route to update a task
 app.put("/tasks/:id", (req, res) => {
-  const id = Number(req.params.id);
+  const id = parseInt(req.params.id);
 
-  const index = tasks.findIndex((task) => task.id === id);
+  const task = db.prepare("SELECT  * FROM tasks WHERE id = ?").get(id);
 
   // Task not found
-  if (index === -1) {
+  if (!task) {
     return res.status(404).json({
       error: `Task ${id} not found`,
     });
@@ -127,29 +109,32 @@ app.put("/tasks/:id", (req, res) => {
     }
   }
 
-  // Update title
-  if (title !== undefined) {
-    tasks[index].title = title.trim();
-  }
+  // actaul updates
+  const updatedTitle = title !== undefined ? title.trim() : task.title;
+  const updatedDone = done !== undefined ? (done ? 1 : 0) : task.done;
 
-  // Update done
-  if (done !== undefined) {
-    tasks[index].done = done;
-  }
+  const stmt = db.prepare("UPDATE tasks SET title = ?, done = ? WHERE id = ?");
+  stmt.run(updatedTitle, updatedDone ? 1 : 0, id);
+  const updatedTask = db.prepare("SELECT * FROM tasks WHERE id = ?").get(id);
 
-  res.status(200).json(tasks[index]);
+  res.status(200).json({
+    id: updatedTask.id,
+    title: updatedTask.title,
+    done: Boolean(updatedTask.done),
+  });
 });
 
 // Delete route to delete a task
 app.delete("/tasks/:id", (req, res) => {
-  let id = Number(req.params.id);
-  const index = tasks.findIndex((task) => task.id === id);
-  if (index === -1) {
+  let id = parseInt(req.params.id);
+  const task = db.prepare("SELECT * FROM tasks WHERE id = ?").get(id);
+  if (!task) {
     return res.status(404).json({
       error: `Task ${id} not found`,
     });
   }
-  tasks.splice(index, 1);
+  const stmt = db.prepare("DELETE FROM tasks WHERE id =?");
+  stmt.run(id);
   res.status(204).send();
 });
 
